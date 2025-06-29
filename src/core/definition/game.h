6033fd848407d78f7fbd4bc584c25d391b371d64
Hongtian Wang <oracynx@gmail.com>
2025-06-29 22:50:10 +0800

    :sparkles: 游戏定义

---
+++ src/core/definition/game.h
@@ -0,0 +1,19 @@
+// 网络通信
+#define PORT 56324
+
+// 游戏数值
+#define HEALTH 6
+#define FLASH_HEALTH 2
+#define MAX_PROP_COUNT 8
+#define PROP_COUNT_GET_ONCE 4
+
+// 游戏卡牌
+#define MAGNIFIER 7001
+#define BEER 7002
+#define CIGARETTE 7003
+#define SAW 7004
+#define HANDCUFFS 7005
+#define INVERTER 7006
+#define PHONE 7007
+#define MEDICINE 7008
+#define ADRENALINE 7009
