4727224998106124ec29bd4d4b41eac93625ca6a
Hongtian Wang <oracynx@gmail.com>
2025-06-29 22:51:34 +0800

    :sparkles: 一些定义

---
+++ src/core/server.h
@@ -0,0 +1,15 @@
+#include "definition/game.h"
+#include "libs/console.h"
+#include "libs/network.h"
+
+// 游戏变量
+class Player
+{
+  public:
+    int health;
+    bool flash;
+};
+
+inline void RunServer()
+{
+}
