162dfce6da6df1e8f400e0dc6b73a7bbc9364ff0
Hongtian Wang <oracynx@gmail.com>
2025-06-30 09:37:20 +0800

    :sparkles: 补充游戏定义

--- src/core/definition/game.h
+++ src/core/definition/game.h
@@ -13,6 +13,8 @@
 #define PROP_COUNT_GET_ONCE 4
 
 // 游戏卡牌
+#define NONE 7000
+
 #define MAGNIFIER 7001
 #define BEER 7002
 #define CIGARETTE 7003
@@ -22,5 +24,7 @@
 #define PHONE 7007
 #define MEDICINE 7008
 #define ADRENALINE 7009
+
+#define PROP_KIND 9
 
 #endif
