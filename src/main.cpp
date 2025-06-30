65b106b58d9145482f8b8bc522991fcfb9948815
Hongtian Wang <oracynx@gmail.com>
2025-06-30 09:36:52 +0800

    :bug: 使用 C++ 风格 IO

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
-#include "core/client.h"
-#include "core/server.h"
-#include <cstdio>
+#include "core/client.cpp"
+#include "core/server.cpp"
+#include <iostream>
 int main()
 {
-    printf("此程序作为服务端还是客户端运行？\n");
-    printf("1. 服务端\n");
-    printf("2. 客户端\n");
-    printf("请输入：");
+    std::cout << "此程序作为服务端还是客户端运行？" << std::endl;
+    std::cout << "1. 服务端" << std::endl;
+    std::cout << "2. 客户端" << std::endl;
+    std::cout << "请输入 > ";
     int mode = 0;
-    scanf("%d", &mode);
+    std::cin >> mode;
     if (mode == 1)
     {
         RunServer();
@@ -19,7 +19,7 @@ int main()
     }
     else
     {
-        printf("[ERROR] 非期望的输出\n");
+        std::cout << "[ERROR] 非期望的输出\n";
     }
     return 0;
 }
